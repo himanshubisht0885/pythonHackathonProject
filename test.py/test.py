6f0badfdbb5376fc0779b0fdcdc3f05cc6923581
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-10 12:18:53 +0530

    buttons

--- test.py/test.py
+++ test.py/test.py
@@ -8,11 +8,14 @@ root.title("test")
 mylabel1=Label(root,text='hello world')
 mylabel2=Label(root,text='hello world12133')
 mylabel3=Label(root,text='                  ')
-#shoving it onto the screen
+#shoving it onto the screen // can you .pack() or .grid()
 mylabel1.grid(row=0,column=0)
 mylabel3.grid(row=1,column=1)
 mylabel2.grid(row=1,column=5)
 mainframe=ttk.Frame(root,padding="3 3 12 12")
-#button=ttk.Button(root,text='hello',command='buttonpressed')
-#button.grid()
+
+
+#creating buttons
+mybutton=Button(root,text='click me')
+mybutton.grid(row=3,column=6)
 root.mainloop()
