9ead3af70b55a062462ab5a3265c0d25327223ee
himanshubisht0885 <87412126+himanshubisht0885@users.noreply.github.com>
2022-12-10 11:49:03 +0530

    button
    tk

--- test.py/test.py
+++ test.py/test.py
@@ -1,2 +1,10 @@
-import tkinter
-tkinter._test()
+from tkinter import *
+from tkinter import ttk
+
+
+root=Tk()
+root.title("test")
+mainframe=ttk.Frame(root,padding="3 3 12 12")
+button=ttk.Button(root,text='hello',command='buttonpressed')
+button.grid()
+root.mainloop()
